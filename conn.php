8a56ea536888c0fd3e27911d9138bcab6ca0a4d0
KitchenLab5 <KitchenLab5@TEKCENTER.LOCAL>
2019-07-23 11:23:06 -0700

    Added PHP code

---
+++ conn.php
@@ -0,0 +1,12 @@
+<?php
+
+//connection file
+$host='localhost';
+$username='Jeff';
+$pass='123456';
+$dbname='website';
+$dsn="mysql:host=$host;dbname=$dbname";
+$options=array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+);
+$conn= new PDO($dsn, $username, $pass, $options);
