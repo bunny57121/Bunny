d6f9bb18d626187cb75f86adbcc3f13632077c40
KitchenLab5 <KitchenLab5@TEKCENTER.LOCAL>
2019-07-23 15:25:44 -0700

    Finished adding sketches

--- movies.php
+++ movies.php
@@ -31,10 +31,10 @@
   <li class="nav-item">
     <a class="nav-link" href="#">Disabled</a>
   </li>
+  </ul>
 
 
-  
-  <p><h1>Monty Python and the holy grail</h1></p>
+ 
     <p><h2>French Taunting</h2></p>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/895eSm4oQRw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <p><h2>Witch Trial</h2></p>
@@ -45,10 +45,10 @@
     <iframe width="560" height="315" src="https://www.youtube.com/embed/TnOdAT6H94s?start=0&end=319" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <p><h2>The Black Knight</h2></p>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/s35rVw1zskA?start=0&end=257" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <p><h1>Life of Brian</h1></p>
     <p><h2>Stoning Scene</h2></p>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/bDe9msExUK8?start=257" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    
+    <p><h2>Latin lesson</h2></p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/KAfKFKBlZbM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 </body>
 </html>
