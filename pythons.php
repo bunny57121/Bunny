0ca63bf93e4e7c324e2152aded10bf16666a0708
KitchenLab5 <KitchenLab5@TEKCENTER.LOCAL>
2019-07-23 10:03:29 -0700

    Update to website

---
+++ pythons.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>The Pythons</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+    <ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link" href="./index.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" href="./pythons.php">Pythons</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="./sketches.php">Sketches</a>
+    </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Movies</a>
+  </li> 
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Live </a>
+  </li>
+</li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Disabled</a>
+  </li>
+</ul>
+
+    <ul>
+ <li><h2>John Cleese</h2></li>
+<img src="https://bloximages.newyork1.vip.townnews.com/pilotonline.com/content/tncms/assets/v3/editorial/5/cf/5cfb4e00-22e9-5335-91d0-87dfb6715a67/5760134890e3a.image.jpg?resize=400%2C253" alt="">
+<li><h2>Terry Gilliam</h2></li>
+<img width="350px" src="https://i.ytimg.com/vi/SQ7JTB2CaN0/hqdefault.jpg" alt="">
+<li><h2>Graham Chapman</h2></li>
+<img src="https://vignette.wikia.nocookie.net/montypython/images/8/87/Grahamchapman.jpg/revision/latest?cb=20110307142528" alt="">
+<li><h2>Eric Idle</h2></li>
+<img src="https://i.pinimg.com/originals/8a/c8/77/8ac877e55c884ad4c5ee945807c24172.jpg" alt="">
+<h2>Michael Palin</h2>
+<img width="400px" src="https://pythonspamclub.com/wp-content/uploads/2018/11/47d5fe29f7b7f538d5bb4fa6f5aa94ab.jpg" alt="">
+<h2>Terry Jones</h2>
+<p><img src="https://www.beyondthejoke.co.uk/sites/beyondthejoke.co.uk/files/styles/large/public/jones.jpg?itok=F5LdA0VW" alt="">
+</p>
+</ul>
+
+
+
+
+    </div>
+</body>
+</html>
---
+++ sketches.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link" href="./index.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="./pythons.php">Pythons</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" href="./sketches.php">Sketches</a>
+    </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Movies</a>
+  </li> 
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Live </a>
+  </li>
+</li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Disabled</a>
+  </li>
+</ul>
+<p><h2>Dead Parrot</h2> </p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/vZw35VUBdzo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p><h2>Spanish Inquisition</h2> </p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/FAxkcPoLYcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p><h2>Upper-class twit of the year show</h2></p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/J1FfrnOXGHg?start=0&end=246" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p><h2>Killer Joke</h2></p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/prDoDPIpX4E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+
+
+</div>    
+</body>
+</html>
